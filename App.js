8f4a584af2be66123a61dacd341b629e49d4ab48
Simas <simonas.deimantavicius@mediapark.com>
2019-09-03 14:12:35 +0300

    test out initial capabilities

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('Text Changed')} />
     </View>
   );
 }
